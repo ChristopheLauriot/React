df0be364b457076e392048bf0d6386a8b0902dc6
Frédéric GAURAT <fgaurat@gmail.com>
2023-11-14 13:39:37 +0100

    Create Concat.jsx

---
+++ hook-app/src/components/Concat/Concat.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+function wait(t){
+    const end = Date.now()+t
+    while(Date.now()<end);
+
+}
+
+function toUpper(v){
+    return v.toUpperCase()
+}
+
+function Concat() {
+    const [name, setName] = useState("the name")
+    const [firstName, setFirstName] = useState("the firstName")
+
+    const doChange = event =>{
+        console.log(event.target.name)
+        const value = event.target.value
+
+        switch(event.target.name){
+            case "name":
+                setName(value)
+                break
+            case "firstName":
+                setFirstName(value)
+                break
+        }
+    }
+    const upperName = toUpper(name)
+    return (
+        <>
+            <h2>Concat</h2>
+            <br/>
+            <input type="text" name="name" value={name} onChange={doChange}/>
+            <input type="text" name="firstName" value={firstName} onChange={doChange}/>
+
+            <br/>
+            Name : {name} 
+            <br/>
+            upperName : {upperName}
+            <br/>
+            firstName : {firstName}
+        </>
+    )
+}
+
+export default Concat
